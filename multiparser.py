7b960ef7bacbc383c56359f44aa35fdede5c0b66
Igor Karabin <nutwood.ki@gmail.com>
2019-05-08 19:07:25 +0300

    2-nd commit -> multiprocessing: with Pool and map()

--- multiparser.py
+++ multiparser.py
@@ -1,15 +1,12 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 import csv
-from multiprocessing import Pool
 import urllib.request
 import urllib.error
-
+from multiprocessing import Pool
 # from urllib.request import urlopen
-
 url = 'http://moviestape.net/katalog_filmiv/'
-
-
 def get_html(url):
     try:
         r = requests.get(url)
@@ -20,14 +17,11 @@ def get_html(url):
         # r = requests.get(url)
         # # print(r.text)
         # return r.text
-
-
 def get_page_count(html):
     soup = BeautifulSoup(html, 'lxml')
     cross_page = soup.find('div', class_='navigation')
     # print (int (cross_page.find_all ('a')[-2].text))
     return int(cross_page.find_all('a')[-2].text)
-
 
 def parse_link(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -36,12 +30,9 @@ def parse_link(html):
     for div in divs:
         a = div.find('p', class_='title').find('a').get('href')
         links.append(a)
-    # print(links)
     return links
 
-
 def parse_film(html):
-    # html = clean_data(html)#---------------- чистка
     soup = BeautifulSoup(html, 'lxml')
     div = soup.find('div', class_='f-box2')
     try:
@@ -53,57 +44,38 @@ def parse_film(html):
     except:
         name_eng = ''
     try:
-        content = div.find('div', class_='f-content2') \
-            .text.replace('\n', '').replace('\u2032', '')
+        content = div.find('div', class_='f-content2').text.replace('\n', '').replace('\u2032', '')
     except:
         content = ''
     try:
-        rating = div.find('div', class_='f-content2_r') \
-            .text.replace('\xa0', '') \
-            .replace('\n', '').replace('\u2032',
-                                       '')
+        rating = div.find('div', class_='f-content2_r').text.replace('\xa0', '') .replace('\n', '').replace('\u2032','')
     except:
         rating = ''
     try:
         script = div.find('div', class_='f-content2_s').text.strip().replace('\u2032', '')
     except:
         script = ''
-
-    img_post = div.find('div', class_='f-poster2').find('img').get('src')
-    url_img_post = 'http://moviestape.net' + img_post
     try:
-        urllib.request.urlretrieve(url_img_post, "test#1\№" + str(n) + '_' +
-                                   name.replace(':', '_')
-                                   .replace('(', '_').replace(')', '')
-                                   .replace('?', '_').rstrip() +
-                                   '_poster.jpg')
-    except urllib.error.HTTPError:
-        print('№' + str(n) + '. Poster not found!!!')
-    imgs = div.find('div', class_='f-content2_ss').find_all('img')
-    for i, img in enumerate(imgs):
-        url_img = 'http://moviestape.net' + img.get('src')
-
-        try:
-            urllib.request.urlretrieve(url_img, "test#1\№" + str(n) + '_' +
-                                       name.replace(':', '_')
-                                       .replace('(', '_').replace(')', '')
-                                       .replace('?', '_').rstrip() +
-                                       '_screen_' + str(i + 1) + '.jpg')
-        except urllib.error.HTTPError:
-            pass
-        except urllib.error.URLError:
-            pass
+        img_post = div.find('div', class_='f-poster2').find('img').get('src')
+    except:
+        img_post = ''
+    try:
+        imgs = div.find('div', class_='f-content2_ss').find_all('img')
+    except:
+        imgs =[]
 
     film = {'Назва фільму': name,
             'Назва(англ.)': name_eng,
             'Виробник': content,
             'Рейтинг': rating[15:],
-            'Зміст': script}
-    # print(film)
-    # name, name_engl, content, rating[15:], descript
+            'Зміст': script,
+            'poster': img_post,
+            'screens': imgs
+            }
     return film
 # def clean_data(text):#----------------------- чистка#
-#     return text.replace('\u2032', '')#------- чистка#
+#     return text.strip().replace('\u2032', '').replace('\n', '').replace('/', '').replace('\xa0', '')\
+
 
 def write_csv(film):
     with open('test#1\my_parser.csv', 'a', newline="", encoding="utf-8") as f:  # E:\Testpython\PythonLabs\
@@ -114,26 +86,50 @@ def write_csv(film):
              film['Виробник'],
              film['Рейтинг'],
              film['Зміст']))
-
+    url_img_post = 'http://moviestape.net' + film['poster']
+    try:
+        urllib.request.urlretrieve(url_img_post, "test#1\№" +'_'
+                                                 + film['Назва фільму'].replace(':', '_').replace('(', '_')
+                                                 .replace(')', '').replace('?', '_').rstrip()
+                                                 + '_poster.jpg')
+    except urllib.error.HTTPError:
+        print('. Poster not found!!!')  # '№' + str(all_links.index(link)+1) + #<<<
+    for i, img in enumerate(film['screens']):
+        url_img = 'http://moviestape.net' + img.get('src')
+        try:
+            urllib.request.urlretrieve(url_img, "test#1\№" +'_'
+                                       + film['Назва фільму'].replace(':', '_').replace('(', '_').replace(')', '')
+                                       .replace('?', '_').rstrip()
+                                       + '_screen_'+str(i + 1)+'.jpg')
+        except urllib.error.HTTPError:
+            pass
+        except urllib.error.URLError:
+            pass
+def make_all(link):
+    html = get_html(link)
+    film = parse_film(html)
+    write_csv(film)
 
 def main():
-    global n
-    n = 1
+    start = datetime.now()
+    global all_links
+    global link
     all_links = []
     page_count = get_page_count(get_html(url))
-    for page in range(1,2):# page_count + 1):
+    for page in range(1, page_count + 1):
         links = parse_link(get_html(url + 'page/%d/' % page))
         for link in links:
             all_links.append(link)
 
-    for num, link in enumerate(all_links):
-        html = get_html(link)
-        film = parse_film(html)
-        n += 1
-        # print('№' + str(num + 1) + '.', film['Назва фільму'])
-        write_csv(film)
-        # with Pool as p:
-        #     p.map(make_all, all_links)
+    # for link in all_links:
+    #     html = get_html(link)
+    #     film = parse_film(html)
+    #     write_csv(film)
+    with Pool(40) as p:
+        p.map(make_all, all_links)
+    finish = datetime.now()
+    total = finish - start
+    print(total)
 
 
 if __name__ == '__main__':
